51a649880bdd896dd42a7ecd76800977e5258f42
DANIELGEDAMO <danielgadamo@gmail.com>
2022-06-15 18:28:52 +0300

    1

---
+++ main.js
@@ -0,0 +1,62 @@
+// function printTextByUserInput(){
+// let num = +
+// user_num.value;
+// switch(num){
+//  case 3:
+//  return console.log('hello');
+//  case 5:
+//  return console.log('goodbye');
+//  case 7:
+//  return console.log('tenks');
+//  default :
+//  return console.log("no masage");
+//  }
+// };
+
+// let nameFrom = user_name;
+// function printUserName() {
+//   switch (nameFrom.value) {
+//     case "jacob":
+//       return myDiv.innerHTML = nameFrom.value;
+//     case "nathan":
+//       return  myDiv.innerHTML = "nathan".toUpperCase();
+//     case "DALYA" :
+//         myDiv.innerHTML = "DALYA".toLowerCase();
+//         return
+
+//     default:
+//       break;
+//   }
+// };
+// function getNumberAndCalculate(){
+//  switch(operator.value) {
+//     case"+":
+//     myDiv.innerHTML = +num_1.value + +num_2.value;
+//        return
+//     case "-":
+//         myDiv.innerHTML = +num_1.value - +num_2.value;
+//        return
+//     case "*":
+//         myDiv.innerHTML = +num_1.value * +num_2.value;
+//        return
+//     case "/":
+//         myDiv.innerHTML = +num_1.value / +num_2.value;
+//         return
+//         ;
+
+//     default:
+//         break;
+//  };
+// };
+// let mycolor = userColor.value
+
+// let mySaize = usersazie.value
+// function printTextColorAndSize() {
+//  myDiv.style.color = userColor.value
+//  myDiv.innerText = userText.value 
+//  myDiv.style.fontSize = userSize.value + "px"
+// };
+// function printElement(){
+
+// }
+
